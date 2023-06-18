329d7a93b43d38b03e3411c1ce681208a0c33ea4
Nick Yonge <nick@curiobot.com>
2023-06-18 15:18:40 -0700

    remove unnecessary code

--- src/mapjson.js
+++ src/mapjson.js
@@ -2,22 +2,6 @@
 import geoEarth from './json/ne_50m_land.geojson';
 import geoOcean from './json/ocean_feature.geojson';
 
-const geoJsonStructure = [
-    [ // [0] = Collections 
-        ['FeatureCollection', ['features']],
-        ['GeometryCollection', ['geometries']]
-    ],
-    [ // [1] = Objects (singular)
-        ['Feature',['geometry', 'properties', 'id']],
-    ]
-];
-
-/** Collection types in GeoJSON format (generally containing collections) */
-const geoJsonCollections = ['FeatureCollection', 'GeometryCollection'];
-/** Single object types in GeoJSON format */
-const geoJsonObjects = ['Feature', 'Point', 'LineString', 'Polygon', 'MultiPoint', 'MultiLineString', 'MultiPolygon'];
-const geoJsonTypes = [...geoJsonCollections, ...geoJsonObjects];
-
 export function GetGeoJSON() {
 
     
