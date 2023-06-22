c9c7ee0614f1e324563d234b6d28fc3bd1f5ee35
Nick Yonge <nick@curiobot.com>
2023-06-21 20:18:31 -0700

    moved RetrieveProjection func into CreateMap func, projectionIndex moved

--- src/mapgen.js
+++ src/mapgen.js
@@ -26,7 +26,6 @@ let maps = [];
 let geojson = GetGeoJSON();
 
 let mapIndex = 0;
-let projectionIndex = 0; // not async 
 
 function CreateMap(module) {
 
@@ -43,7 +42,8 @@ function CreateMap(module) {
         return;
     }
 
-    projectionIndex = 0;
+    let projectionIndex = 0;
+    let loadedProjections = 0;
 
     // mapContainer.appendChild(title);
 
@@ -87,160 +87,169 @@ function CreateMap(module) {
 
     maps.push(mapData);
 
-}
+    function RetrieveProjection(projectionType, mapData) {
 
-function RetrieveProjection(projectionType, mapData) {
+        if (mapData == null) {
+            console.error("null mapData, can't retrieve projection, projectionType: " + projectionType);
+            return;
+        }
 
-    if (mapData == null) {
-        console.error("null mapData, can't retrieve projection, projectionType: " + projectionType);
-        return;
-    }
+        let map = mapData.map;
 
-    let map = mapData.map;
+        if (projectionType == null || projectionType == "") {
+            console.error('null/empty projection type specified, cannot retrieve');
+            return;
+        }
+        if (map == null || map == "") {
+            console.error('null/empty map projection specified, cannot create, ' +
+                'ensure map is first defined by calling CreateMap');
+            return;
+        }
 
-    if (projectionType == null || projectionType == "") {
-        console.error('null/empty projection type specified, cannot retrieve');
-        return;
-    }
-    if (map == null || map == "") {
-        console.error('null/empty map projection specified, cannot create, ' +
-            'ensure map is first defined by calling CreateMap');
-        return;
-    }
+        // checks passed, begin loading 
+        loadedProjections++;// increment load counter
 
-    console.log('retrieving projection ' + projectionType + ' for map projection ' + map);
-    let projection = GetProjection(projectionType);
+        console.log('retrieving projection ' + projectionType + ' for map projection ' + map);
+        let projection = GetProjection(projectionType);
 
-    let fitSize = 1;
-    // adjust projection scale (zoom)
-    switch (map) {
-        case m.grieger:
-            fitSize = containerScale;
-            break;
-    }
-    projection
-        .fitSize([mapSize * fitSize, mapSize * fitSize], overlayGeo);
-    // using ocean JSON here because it extends all the way to the edges of lat/long, -180/-90 to 180/90, eg max size
-
-
-    // create geopath generator
-    let geoGenerator = d3.geoPath()
-        .projection(projection);
-
-    // left offset (adjust first map size - use projection width for right map cutoff)
-    let leftOffset = 0;
-    switch (map) {
-        case m.grieger:
-            if (projectionIndex == 0) {
-                leftOffset = mapSize * -0.5;
-            }
-            break;
-    }
-
-    // prepare relevant transformations 
-    let transform;
-
-    let applyTransformation = false;
-    let rotation = 0;
-    let translationX = 0;
-    let translationY = 0;
+        let fitSize = 1;
+        // adjust projection scale (zoom)
+        switch (map) {
+            case m.grieger:
+                fitSize = containerScale;
+                break;
+        }
+        projection
+            .fitSize([mapSize * fitSize, mapSize * fitSize], overlayGeo);
+        // using ocean JSON here because it extends all the way to the edges of lat/long, -180/-90 to 180/90, eg max size
 
 
-    // check for / apply per-map transformations
-    // switch (map) { // obsolete - keeping for ref 
-    //     case 'grieger-test':
-    //         applyTransformation = true;
-    //         rotation = 90;
-    //         // translationX = -(mapSize * (containerScale - 1) * containerOffset);
-    //         translationY = mapSize;
-    //         break;
-    // }
+        // create geopath generator
+        let geoGenerator = d3.geoPath()
+            .projection(projection);
 
-    // check for / apply per-projection transformations
-    switch (projectionType) {
-        case m.adams1:
-        case m.adams2:
-            applyTransformation = true;
-            // horizontal, a1 NA facing up
-            rotation = 135;
-            translationX = (mapSize * (containerScale - 1) * containerOffset);
-            translationY = mapSize;
-            // vertical, a1 NA facing right
-            // rotation = 225;
-            // translationX = mapSize;
-            // translationY = mapSize * (containerScale - 1) * containerOffset;
-            break;
-    }
+        // left offset (adjust first map size - use projection width for right map cutoff)
+        let leftOffset = 0;
+        switch (map) {
+            case m.grieger:
+                if (projectionIndex == 0) {
+                    leftOffset = mapSize * -0.5;
+                }
+                break;
+        }
 
-    // if projection calls for transformation, apply it here
-    if (applyTransformation) {
-        transform =
-            "rotate(" + rotation + "," + mapSize + "," + mapSize + ") " +
-            "translate(" + translationX + "," + translationY + ")";
-    }
+        // prepare relevant transformations 
+        let transform;
+
+        let applyTransformation = false;
+        let rotation = 0;
+        let translationX = 0;
+        let translationY = 0;
 
 
-    let svgContainerId = "map_" + map + "_projection_" + projectionIndex;
+        // check for / apply per-map transformations
+        // switch (map) { // obsolete - keeping for ref 
+        //     case 'grieger-test':
+        //         applyTransformation = true;
+        //         rotation = 90;
+        //         // translationX = -(mapSize * (containerScale - 1) * containerOffset);
+        //         translationY = mapSize;
+        //         break;
+        // }
 
-    // create the svg for the map
-    let svg = d3.select(mapData.projectionsContainer).append('svg')
-        .attr("class", "map")
-        .attr("id", svgContainerId)
-        .attr("width", mapSize)
-        .attr("height", mapSize)
-        .style("margin-left", leftOffset + 'px');
-    
-    
+        // check for / apply per-projection transformations
+        switch (projectionType) {
+            case m.adams1:
+            case m.adams2:
+                applyTransformation = true;
+                // horizontal, a1 NA facing up
+                rotation = 135;
+                translationX = (mapSize * (containerScale - 1) * containerOffset);
+                translationY = mapSize;
+                // vertical, a1 NA facing right
+                // rotation = 225;
+                // translationX = mapSize;
+                // translationY = mapSize * (containerScale - 1) * containerOffset;
+                break;
+        }
 
-    // apply data
-    let g = svg.append('g')
-        .attr("transform", transform);
+        // if projection calls for transformation, apply it here
+        if (applyTransformation) {
+            transform =
+                "rotate(" + rotation + "," + mapSize + "," + mapSize + ") " +
+                "translate(" + translationX + "," + translationY + ")";
+        }
 
-    g.selectAll('path')
-        .data(geojson.features)
-        .enter();
 
-    // apply data
-    g = svg.append('g')
-        .attr("transform", transform)
-        .selectAll('path')
-        .data(geojson.features)
-        .enter();
+        let svgContainerId = "map_" + map + "_projection_" + projectionIndex;
 
-    g.append('path')
-        .attr('class', function (d) {
-            return 'map' + (d.properties && d.properties.water === true ? ' water' : ' land');
-        })
-        .attr('d', geoGenerator);
-
-    // per-map border
-    if (usePerMapBorder) {
-        var containerBorder = svg.append("rect")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("height", mapSize)
+        // create the svg for the map
+        let svg = d3.select(mapData.projectionsContainer).append('svg')
+            .attr("class", "map")
+            .attr("id", svgContainerId)
             .attr("width", mapSize)
-            .style("stroke", "#FF0000")
-            .style("fill", "none")
-            .style("stroke-width", 2)
-            ;
+            .attr("height", mapSize)
+            .style("margin-left", leftOffset + 'px');
+
+        // per-map svg border
+        if (usePerMapBorder) {
+            var containerBorder = svg.append("rect")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("height", mapSize)
+                .attr("width", mapSize)
+                .style("stroke", "#FF0000")
+                .style("fill", "none")
+                .style("stroke-width", 2)
+                ;
+        }
+
+        // apply data
+        let g = svg.append('g')
+            .attr("transform", transform);
+
+        g.selectAll('path')
+            .data(geojson.features)
+            .enter();
+
+        // apply data
+        g = svg.append('g')
+            .attr("transform", transform)
+            .selectAll('path')
+            .data(geojson.features)
+            .enter();
+
+        g.append('path')
+            .attr('class', function (d) {
+                return 'map' + (d.properties && d.properties.water === true ? ' water' : ' land');
+            })
+            .attr('d', geoGenerator);
+
+
+        // get this projection container
+        let svgContainer = document.getElementById(svgContainerId);
+
+        // create projection data container
+        let projectionData = new ProjectionData(
+            projection, projectionIndex, svgContainer, svg, mapSize, mapData);
+
+        // apply map events
+        AssignInput(projectionData);
+
+        // add projectiondata to mapdata 
+        mapData.AddProjection(projectionData);
+
+        // decrement projection load index 
+        loadedProjections--;
+
+        if (loadedProjections == 0) {
+            // done loading projections! 
+            console.log("Done!");
+        }
+
+        // increment projection index 
+        projectionIndex++;
     }
-
-    // get this projection container
-    let svgContainer = document.getElementById(svgContainerId);
-
-    // create projection data container
-    let projectionData = new ProjectionData(
-        projection, projectionIndex, svgContainer, svg, mapSize, mapData);
-
-    // apply map events
-    AssignInput(projectionData);
-
-    // increment projection index 
-    projectionIndex++;
-
-    // add projectiondata to mapdata 
-    mapData.AddProjection(projectionData);
 }
 
 /**
