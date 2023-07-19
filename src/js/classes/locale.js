c99787108d0fe565755a5d84b8c7a0c6a47f66d2
Nick Yonge <nick@curiobot.com>
2023-07-19 14:09:55 -0700

    geoPoint is now abstraction of lat/long

--- src/js/classes/locale.js
+++ src/js/classes/locale.js
@@ -3,20 +3,20 @@ export class Locale {
 
     constructor(...localeData) {
 
-        
+
         this.lat = 0;
         this.long = 0;
         this.geoPoint = [lat, long];
-        
+
         this.city = null;
         this.region = null;
         this.country = null;
         this.continent = null;
-        
+
         if (localeData == null)
     }
 
-    
+
     get lat() {
         return this._lat;
     }
@@ -24,7 +24,7 @@ export class Locale {
         return this._long;
     }
     get geoPoint() {
-        return this._geoPoint;
+        return [this._lat, this._long];
     }
     get city() {
         return this._city;
@@ -38,7 +38,7 @@ export class Locale {
     get continent() {
         return this._continent;
     }
-    
+
     set lat(lat) {
         this._lat = lat;
     }
@@ -46,7 +46,29 @@ export class Locale {
         this._long = long;
     }
     set geoPoint(geoPoint) {
-        this._geoPoint = geoPoint;
+        if (geoPoint == null) {
+            this._lat = null;
+            this._long = null;
+            return;
+        }
+        if (!Array.isArray(geoPoint)) {
+            console.warn("GeoPoint must be an array, cannot assign",
+                geoPoint, "as a value, returning");
+            return;
+        }
+        if (geoPoint.length != 2) {
+            console.warn("GeoPoint array must have two values, cannot assign",
+                geoPoint, "returning");
+        }
+        if (
+            (geoPoint[0] != null && typeof (geoPoint[0]) !== 'number') ||
+            (geoPoint[1] != null && typeof (geoPoint[1] !== 'number'))) {
+            console.warn("One or both of GeoPoint's values are invalid,",
+                geoPoint, "values must be null or type number, returning");
+            return;
+        }
+        this._lat = geoPoint[0];
+        this._long = geoPoint[1];
     }
     set city(city) {
         this._city = city;
