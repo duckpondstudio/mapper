fab0f223e59926109390cc89e5d294fb40d56c7b
Nick Yonge <nick@curiobot.com>
2023-07-22 15:13:29 -0700

    creating script to identify geo text vals

---
+++ src/js/classes/datanames.js
@@ -0,0 +1,28 @@
+
+// extra considerations: "xyz_Label", "xyz-label"
+
+/**
+ * Array of common names for latitude (all lowercase)
+ */
+const namesLatitutde = [
+    'latitude', 'la', 'lat', 'lt', 'ltd', 'lttd'
+];
+/**
+ * Array of common typos for latitude (all lowercase)
+ */
+const typosLatitude = [
+    'lattitude', 'latitute', 'latidude', 'latittude', 'lattittude'
+];
+/**
+ * Array of common names for longitude (all lowercase)
+ */
+const namesLongitude = [
+    'longitude', 'lo', 'ln', 'lg', 'lon', 'lng', 'long', 'lngt',
+    'lngd', 'lngtd', 'longi', 'longtd', 'lgt', 'lgtd', 'lgd'
+];
+/**
+ * Array of common typos for longitude (all lowercase)
+ */
+const typosLongitude = [
+    'longitute', 'longidude', 'longittude'
+];
