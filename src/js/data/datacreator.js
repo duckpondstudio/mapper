23c1d16d59b36e8f51882b47827fb7bb7b619292
Nick Yonge <nick@curiobot.com>
2023-07-28 12:27:06 -0700

    genericized data creator, prep for all other types

--- src/js/data/datacreator.js
+++ src/js/data/datacreator.js
@@ -7,59 +7,80 @@ let debugDataCreator = true;
 
 let currentCSV;
 let csvParseCount = 0;
+let csvSuccessCount = 0;
 
 export function BuildContinents() {
-
     currentCSV = new CSVData('all_continents',
         'name', 'code', 'm49', 'alt_names');
-
-    ParseCSV('continent-codes', ParseCSVContinents, ParseCSVError, 1);
-
+    ParseCSV('continent-codes', ParseCSVSuccess, ParseCSVError, 'continents', 1);
 }
 
-function ParseCSV(localFileName, onComplete, onError, callbackParam = null) {
+function ParseCSV(localFileName, onComplete, onError, type, step) {
     csvParseCount++;
-    ParseCSVLocal(localFileName, onComplete, onError, callbackParam);
+    ParseCSVLocal(localFileName, onComplete, onError, { type: type, step: step });
 }
 
-function ParseCSVContinents(results, file, callbackParam = null) {
+function ParseCSVSuccess(results, file, callbackParam = null) {
     console.log("Parsed CSV Continents file:", file, ', Callback Param:', callbackParam);
     if (debugDataCreator) {
         console.log("CSV file results:", results);
     }
     console.log(1);
-    switch (callbackParam) {
-        case 1:
-            console.log(2);
-            for (let i = 1; i < results.data.length; i++) {
-                console.log(3 + ',', i);
-                currentCSV.AddRow(
-                    results.data[i][2],
-                    results.data[i][0],
-                    results.data[i][1],
-                    results.data[i][3]
-                );
+    switch (callbackParam.type) {
+        case 'continents':
+            switch (callbackParam.step) {
+                case 1:
+                    console.log(2);
+                    for (let i = 1; i < results.data.length; i++) {
+                        console.log(3 + ',', i);
+                        currentCSV.AddRow(
+                            results.data[i][2],
+                            results.data[i][0],
+                            results.data[i][1],
+                            results.data[i][3]
+                        );
+                    }
+                    break;
+            }
+            break;
+        case 'countries':
+            switch (callbackParam.step) {
+                case 1:
+                    break;
+            }
+            break;
+        case 'regions':
+            switch (callbackParam.step) {
+                case 1:
+                    break;
+            }
+            break;
+        case 'cities':
+            switch (callbackParam.step) {
+                case 1:
+                    break;
             }
             break;
     }
-    csvParseCount--;
-    if (csvParseCount == 0) {
-        // done
-        currentCSV.Save();
-    }
-}
-function ParseCSVCountries(results, file, callbackParam = null) {
-
-}
-function ParseCSVRegions(results, file, callbackParam = null) {
-
-}
-function ParseCSVCities(results, file, callbackParam = null) {
-
+    csvSuccessCount++;
+    CSVParseDecrement();
 }
 
 function ParseCSVError(error, file) {
     console.error("Error", error, "while parsing CSV file", file);
+    CSVParseDecrement();
+}
+
+function CSVParseDecrement() {
+    csvParseCount--;
+    if (csvParseCount == 0) {
+        // done
+        if (csvSuccessCount > 0) {
+            currentCSV.Save();
+        } else {
+            console.error("No successful CSV files were parsed, nothing to write");
+        }
+    }
 }
 
 
@@ -80,8 +101,8 @@ class CSVData {
         if (row.length != this.rowLabels.length) {
             if (debugDataCreator) {
                 console.warn("Row length", row.length, "does not match rowLabels length",
-                this.rowLabels.length, "adding/culling values. RowLabels:", this.rowLabels,
-                ", Row:", row);
+                    this.rowLabels.length, "adding/culling values. RowLabels:", this.rowLabels,
+                    ", Row:", row);
             }
             row.length = this.rowLabels.length;
         }
