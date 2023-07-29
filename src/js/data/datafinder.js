0725210f459d8cf06f6884cabb03aeca68e0984f
Nick Yonge <nick@curiobot.com>
2023-07-29 08:54:30 -0700

    prep finder class for getting location info by other info

---
+++ src/js/data/datafinder.js
@@ -0,0 +1,76 @@
+//#region Continents
+
+//#region MAIN
+// MAIN: returns name
+// (eg GetContinentByX)
+
+//#endregion MAIN
+
+
+//#region SUB
+// SUB: returns non-name info
+// (eg GetContinentM49Code)
+
+//#endregion SUB
+
+//#endregion Continents
+
+
+
+
+//#region Countries
+
+//#region MAIN
+// MAIN: returns name
+// (eg GetCountryByX)
+
+//#endregion MAIN
+
+
+//#region SUB
+// SUB: returns non-name info
+// (eg GetCountryIso3Code)
+
+//#endregion SUB
+
+//#endregion Countries
+
+
+
+
+//#region Regions (Admin 1)
+
+//#region MAIN
+// MAIN: returns name
+// (eg GetRegionByX)
+
+//#endregion MAIN
+
+
+//#region SUB
+// SUB: returns non-name info
+// (eg GetRegionA1Code)
+
+//#endregion SUB
+
+//#endregion Regions (Admin 1)
+
+
+
+
+//#region Cities
+
+//#region MAIN
+// MAIN: returns name
+// (eg GetCityByX)
+
+//#endregion MAIN
+
+
+//#region SUB
+// SUB: returns non-name info
+// (eg GetCityAdmin2Code)
+
+//#endregion SUB
+
+//#endregion Cities
