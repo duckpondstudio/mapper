cd51071646fbbdf1508733f851f0026c4c5f898a
Nick Yonge <nick@curiobot.com>
2023-08-11 15:43:05 -0700

    dropdown autodetect separator / invalid entries

--- src/js/ui/dropdown.js
+++ src/js/ui/dropdown.js
@@ -15,8 +15,21 @@ export function CreateDropdown(name, id, parent, onChangeCallback, ...options) {
     dropdown.name = name;
     dropdown.id = id;
 
-    for (const val of options) {
+    for (let val of options) {
         var option = document.createElement("option");
+        val = val.trim();
+        let disabled = false;
+        if (val == '---') {
+            disabled = true;
+            val = ' - - - - - ';
+        }
+        else if (val.startsWith('--')) {
+            while (val.startsWith('-'))
+                val = val.substring(1);
+            val = ' - - - ' + val.trim() + ' - - - ';
+            disabled = true;
+        }
+        option.disabled = disabled;
         option.value = val;
         if (capitalizeFirstLetter) {
             option.text = val.charAt(0).toUpperCase() + val.slice(1);
