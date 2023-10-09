f504610718358dfd2d12bef6c8c9acc7d3c9e48f
Nick Yonge <nick@curiobot.com>
2023-10-09 14:14:20 -0700

    IsDark luminance color check

--- src/js/utils/color.js
+++ src/js/utils/color.js
@@ -23,6 +23,42 @@ export function RGBAToHexA(rgba, forceRemoveAlpha = true) {
         .join("") // Puts the array to togehter to a string
 }
 
+/**
+ * Is the given color considered "dark" (closer to black than white)?
+ * @param {string} color Hex or RGB(A) color  
+ * @param {Number} cutoff Numeric value for darkness threshold, 0 (black) to 255 (white) 
+ * @returns true if dark, false if light
+ */
+export function IsDark(color, cutoff = 127.5) {
+    // credit: https://awik.io/determine-color-bright-dark-using-javascript/
+    // Variables for red, green, blue values
+    var r, g, b, hsp;
+    // Check the format of the color, HEX or RGB?
+    if (color.match(/^rgb/)) {
+        // If RGB --> store the red, green, blue values in separate variables
+        color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+        r = color[1];
+        g = color[2];
+        b = color[3];
+    }
+    else {
+        // If hex --> Convert it to RGB: http://gist.github.com/983661
+        color = +("0x" + color.slice(1).replace(
+            color.length < 5 && /./g, '$&$&'));
+        r = color >> 16;
+        g = color >> 8 & 255;
+        b = color & 255;
+    }
+    // HSP equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(
+        0.299 * (r * r) +
+        0.587 * (g * g) +
+        0.114 * (b * b)
+    );
+    // Using the HSP value, determine whether the color is light or dark
+    return hsp <= cutoff;
+}
+
 
 // --------------------- //
 // --- SINGLE COLORS --- //
